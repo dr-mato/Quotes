71bb4ecd7a8142f0de4b2dc8e2879c741476c456
dr-mato <lukamatchavariani60@gmail.com>
2023-05-01 14:48:34 +0400

    differentproject

--- src/App.js
+++ src/App.js
@@ -1,8 +1,102 @@
-import MyComponent from './HW-6/HW-6';
-function App() {
+import React,{useState} from "react";
+
+const listQ = 
+    [
+        {
+        quote: "The only way to do great work is to love what you do.",
+        author: "Steve Jobs",
+        },
+        {
+        quote:
+        "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+        author: "Nelson Mandela",
+        },
+        {
+        quote:
+        "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+        author: "Winston Churchill",
+        },
+        {
+        quote: "Believe you can and you're halfway there.",
+        author: "Theodore Roosevelt",
+        },
+        {
+        quote:
+        "Don't judge each day by the harvest you reap but by the seeds that you plant.",
+        author: "Robert Louis Stevenson",
+        },
+        {
+        quote:
+        "The future belongs to those who believe in the beauty of their dreams.",
+        author: "Eleanor Roosevelt",
+        },
+        {
+        quote:
+        "In three words I can sum up everything I've learned about life: it goes on.",
+        author: "Robert Frost",
+        },
+        {
+        quote:
+        "Do not go where the path may lead, go instead where there is no path and leave a trail.",
+        author: "Ralph Waldo Emerson",
+        },
+        {
+        quote: "Life is really simple, but we insist on making it complicated.",
+        author: "Confucius",
+        },
+        {
+        quote:
+        "The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart.",
+        author: "Helen Keller",
+        },
+        {
+        quote: "The best way to predict the future is to invent it.",
+        author: "Alan Kay",
+        },
+        {
+        quote: "Life is 10% what happens to you and 90% how you react to it.",
+        author: "Charles R. Swindoll",
+        },
+    ];
+
+function App(){
+  const [Data, setData] = useState([listQ[0]]);
+  
+  const deleteQs = () => {
+    setData([]);
+  };
+
+  const getQ = () => {
+    let newQuote = listQ[Math.floor(Math.random() * listQ.length)];
+    while (Data && Data.includes(newQuote)){
+      newQuote = listQ[Math.floor(Math.random() * listQ.length)]
+    }
+    setData([...Data, newQuote]);
+  };
+
+  const deleteQ = (ind) => {
+    const newL = [...Data];
+    newL.splice(ind, 1);
+    setData(newL);
+  };
+
+
   return (
     <div className="App">
-      <MyComponent></MyComponent>
+      <button onClick={deleteQs} className="dAll">  Delete all </button>
+      <div>
+      {Data.map((qu, ind) => (
+        <div key={ind} className="container">
+          <p className='qu'>{qu.quote}</p>
+          <p className='author'>-{qu.author}</p>
+          <div className='buttons'>
+          <button className='get' onClick={getQ}>Get a new quote</button>
+          <button className='delete' onClick={() => deleteQ(ind)}>Delete</button>
+          </div> 
+        </div>
+      ))}
+      </div>
+
     </div>
   );
 }
